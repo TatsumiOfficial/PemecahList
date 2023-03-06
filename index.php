d7a81f787cf9dbeeb167945bf1242ca6ecaead7b
Tatsumi-Crew Official <71838907+TatsumiOfficial@users.noreply.github.com>
2023-03-06 13:30:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,57 +31,25 @@
   });
 </script>
 <?php
-//header('Access-Control-Allow-Headers: *');
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-//error_reporting(E_ALL);
-//ini_set('display_errors', 'On');
-ini_set('max_execution_time', 300);
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-define('LARAVEL_START', microtime(true));
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
- */
-require __DIR__ . '/../vendor/autoload.php';
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
- */
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
- */
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
-$response->send();
-$kernel->terminate($request, $response);
+
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
+
